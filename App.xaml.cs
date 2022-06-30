92c2644298a83657339c126046ede61f23e69b5a
CristianFea <30725347+CristianFea@users.noreply.github.com>
2022-06-30 12:23:58 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,113 @@
+﻿using FEAGROPilot_v22.Controladores;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace pruebaUserControl
+{
+   
+    sealed partial class App : Application
+    {
+        public static SolidColorBrush blanco = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+        public static SolidColorBrush verdeclaro = new SolidColorBrush(Color.FromArgb(255, 116, 255, 0));
+        public static SolidColorBrush verde = new SolidColorBrush(Windows.UI.Colors.Green);
+        public static SolidColorBrush amarillo = new SolidColorBrush(Windows.UI.Colors.Yellow);
+        public static SolidColorBrush naranja = new SolidColorBrush(Color.FromArgb(255, 243, 97, 41));
+        public static SolidColorBrush rojo = new SolidColorBrush(Windows.UI.Colors.Red);
+        public static SolidColorBrush negro = new SolidColorBrush(Windows.UI.Colors.Black);
+        public static SolidColorBrush gris = new SolidColorBrush(Windows.UI.Colors.Gray);
+        public static SolidColorBrush transparente = new SolidColorBrush(Windows.UI.Colors.Transparent);
+        public static SolidColorBrush medioNegro = new SolidColorBrush(Windows.UI.Color.FromArgb(125, 0, 0, 0));
+        public static SolidColorBrush fondo = new SolidColorBrush(Color.FromArgb(200, 180, 180, 180));// = "#CCB4B4B4";
+        public static SolidColorBrush azul = new SolidColorBrush(Color.FromArgb(255, 12, 26, 171));
+        public static SolidColorBrush agua = new SolidColorBrush(Windows.UI.Colors.Aqua);
+        public static App Corriente;
+        public App()
+        {
+            this.InitializeComponent();
+            Corriente = this;
+            this.Suspending += OnSuspending;
+            GPIO.IniciarGpio();
+            Mcp2515.arrancarMcp2515();
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// de entrada cuando la aplicación se inicie para abrir un archivo específico, por ejemplo.
+        /// </summary>
+        /// <param name="e">Información detallada acerca de la solicitud y el proceso de inicio.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // No repetir la inicialización de la aplicación si la ventana tiene contenido todavía,
+            // solo asegurarse de que la ventana está activa.
+            if (rootFrame == null)
+            {
+                // Crear un marco para que actúe como contexto de navegación y navegar a la primera página.
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Cargar el estado de la aplicación suspendida previamente
+                }
+
+                // Poner el marco en la ventana actual.
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Cuando no se restaura la pila de navegación, navegar a la primera página,
+                    // configurando la nueva página pasándole la información requerida como
+                    //parámetro de navegación
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Asegurarse de que la ventana actual está activa.
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// </summary>
+        /// <param name="sender">Marco que produjo el error de navegación</param>
+        /// <param name="e">Detalles sobre el error de navegación</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Se invoca al suspender la ejecución de la aplicación. El estado de la aplicación se guarda
+        /// sin saber si la aplicación se terminará o se reanudará con el contenido
+        /// de la memoria aún intacto.
+        /// </summary>
+        /// <param name="sender">Origen de la solicitud de suspensión.</param>
+        /// <param name="e">Detalles sobre la solicitud de suspensión.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Guardar el estado de la aplicación y detener toda actividad en segundo plano
+            deferral.Complete();
+        }
+    }
+}
---
+++ Avisos.xaml.cs
@@ -0,0 +1,101 @@
+﻿using FEAGROPilot_v22.Controladores;
+using pruebaUserControl;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace FEAGROPilot_v22
+{
+    public sealed partial class Avisos : UserControl
+    {
+        public enum Aviso
+        {
+            Info,
+            Aviso,
+            _Aviso,
+            Advertencia,
+            Atencion,
+            Cuidado,
+            Peligro
+        }
+        public Aviso aviso;
+        public string strImagen = "";
+        public string strMsj = "";
+        SolidColorBrush letrasSup;
+        SolidColorBrush color;
+        SolidColorBrush colorInf;
+        private  Popup popup;
+
+        public static async void Mostrar(string MensajeError, string ImgAlerta, int numErr)
+        {
+            try
+            {
+                var s = new Avisos();
+                s.aviso = (Aviso)numErr;
+                s.strImagen = "ms-appx:///Imagenes/Advertencias/" + ImgAlerta + ".png";
+                s.strMsj = MensajeError;                
+                s.popup.IsOpen = true;
+                await Task.Delay(5000);
+                s.popup.IsOpen = false;
+            }
+            catch {  }
+        }
+        public Avisos()
+        {            
+            color = Color((int)aviso);
+            if ((int)aviso == 3 || (int)aviso == 4)
+            {
+                colorInf = App.amarillo;
+                letrasSup = App.negro;
+            }
+            else
+            {
+                colorInf = App.fondo;
+                letrasSup = App.blanco;
+            }
+            beep((int)aviso);
+            InitializeComponent();
+            popup = new Popup();
+            popup.Child = this;
+        }      
+        void beep(int valor)
+        {
+            switch (valor)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    GPIO.Beep(2);
+                    GPIO.LedAlerta(2);
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    GPIO.Beep(3);
+                    GPIO.LedAlerta(3);
+                    break;
+            }
+        }
+        public SolidColorBrush Color(int Valor)
+        {
+            switch (Valor)
+            {
+                case 0:
+                case 1:
+                    return App.verde;
+                case 2:
+                    return App.azul;
+                case 3:
+                case 4:
+                    return App.amarillo;
+                case 5:
+                case 6:
+                    return App.rojo;
+                default:
+                    return App.verde;
+            }
+        }
+    }
+}
