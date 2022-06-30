501ecd8f5b2f8b0dfd36b9e505f2d187040c816b
CristianFea <30725347+CristianFea@users.noreply.github.com>
2022-06-30 12:32:14 -0300

    Add files via upload

---
+++ Guiado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using FEAGROPilot_v22.Controladores;
+
+namespace FEAGROPilot_v22.CodigoGeneral
+{
+    class Guiado
+    {
+        public static void Act_Desac()
+        {
+            //Task.Run(() => GPIO.Beep(1));
+            Avisos.Mostrar("Acerquese a la linea", "Rombo", 2);
+        }
+    } 
+}
